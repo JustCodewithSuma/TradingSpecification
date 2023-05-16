6e74e5f48590165fc0d6ce7f2edd58d8c9e48048
JustCodewithSuma <116901841+JustCodewithSuma@users.noreply.github.com>
2023-05-16 17:52:12 +0200

    Add files via upload

---
+++ src/main/java/com/code/challenge/trading/BasicAuthWebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.code.challenge.trading.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class BasicAuthWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/trading").hasRole("USER").anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable();;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
